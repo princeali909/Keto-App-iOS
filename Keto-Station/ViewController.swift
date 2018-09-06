64cf48379e4a80c8ea933178223f2beadb5e414f
My Mac <mymac@mys-macbook-pro.local>
2018-09-06 12:06:00 -0700

    Initial Commit

---
+++ Keto-Station/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Keto-Station
+//
+//  Created by Ali Halawa on 9/6/18.
+//  Copyright © 2018 Ali Halawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
